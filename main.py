309086785dcd7bed8fc4a84850786710bec9373b
Rugma Purushothaman <rugmapurushothaman00@gmail.com>
2026-08-06 11:46:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-print("=" * 50)
+from detectors.hidden_files import detect_hidden_files
+
+print("=" * 60)
 print("ANTI-FORENSICS DETECTION TOOLKIT")
-print("=" * 50)
+print("=" * 60)
 
 print("\nStarting Scan...\n")
+
+scan_path = "sample_data"
+
+hidden = detect_hidden_files(scan_path)
+
+if hidden:
+    print("[!] Hidden files detected:")
+    for file in hidden:
+        print(file)
+else:
+    print("[✓] No hidden files found.")
